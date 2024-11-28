98a3172ddda1bd11b1a5a5ef596b357cb72939d1
mozartCuman <mozartcumana@gmail.com>
2024-11-28 15:05:41 -0300

    Atualização do CRUD com MySQL

--- src/main/java/com/blogpessoal/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/blogpessoal/blogpessoal/repository/PostagemRepository.java
@@ -1,10 +1,14 @@
 package com.blogpessoal.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.blogpessoal.blogpessoal.model.Postagem;
 
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
